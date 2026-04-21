1c8b7d51538e16cf118a3d2eee351bcb237f4926
Karen Comandante <kcomandante@ssct.edu.ph>
2026-04-21 17:40:05 +0800

    Update receipt.java

--- receipt.java
+++ receipt.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;  
-   
+    
 public class receipt {
     public static void main(String[] args) {        
         System.out.println("GAISANO MALL \nNational HWY., KM4 \nBrgy.Luna, Surigao City\nSurigao Del Norte");        
