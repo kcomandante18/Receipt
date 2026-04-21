f46e720bafc19c91c1a96eaf13c1279e2c9152b3
Karen Comandante <kcomandante@ssct.edu.ph>
2026-04-21 17:39:49 +0800

    Update receipt.java

--- receipt.java
+++ receipt.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
    
 public class receipt {
     public static void main(String[] args) {       
-        System.out.println("GAISANO MALL \nNational HWY., KM4 \nBrgy.Luna, Surigao City\nSurigao Del Norte");       
+        System.out.println("GAISANO MALL \nNational HWY., KM4 \nBrgy.Luna, Surigao City\nSurigao Del Norte");        
         System.out.println("\n\n==========================================================================\n\n");        
     
         Scanner scan = new Scanner(System.in);    
